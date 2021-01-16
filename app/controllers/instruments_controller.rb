e2ee7c1f94bbe60e7b0c4479fd1dbc04e5174a7f
mstva <mstva@gmail.com>
2021-01-16 15:54:33 +0200

    setup instrument index and show

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,34 +1,26 @@
 class InstrumentsController < ApplicationController
   before_action :set_instrument, only: [:show, :edit, :update, :destroy]
 
-  # GET /instruments
-  # GET /instruments.json
   def index
     @instruments = Instrument.all
   end
 
-  # GET /instruments/1
-  # GET /instruments/1.json
   def show
   end
 
-  # GET /instruments/new
   def new
-    @instrument = Instrument.new
+    @instrument = current_user.instruments.build
   end
 
-  # GET /instruments/1/edit
   def edit
   end
 
-  # POST /instruments
-  # POST /instruments.json
   def create
-    @instrument = Instrument.new(instrument_params)
+    @instrument = current_user.instruments.build(instrument_params)
 
     respond_to do |format|
       if @instrument.save
-        format.html { redirect_to @instrument, notice: 'Instrument was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Instrument was successfully created.' }
         format.json { render :show, status: :created, location: @instrument }
       else
         format.html { render :new }
@@ -37,12 +29,10 @@ class InstrumentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /instruments/1
-  # PATCH/PUT /instruments/1.json
   def update
     respond_to do |format|
       if @instrument.update(instrument_params)
-        format.html { redirect_to @instrument, notice: 'Instrument was successfully updated.' }
+        format.html { redirect_to root_path, notice: 'Instrument was successfully updated.' }
         format.json { render :show, status: :ok, location: @instrument }
       else
         format.html { render :edit }
@@ -51,8 +41,6 @@ class InstrumentsController < ApplicationController
     end
   end
 
-  # DELETE /instruments/1
-  # DELETE /instruments/1.json
   def destroy
     @instrument.destroy
     respond_to do |format|
@@ -69,6 +57,6 @@ class InstrumentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def instrument_params
-      params.require(:instrument).permit(:title, :description, :model, :brand, :price, :condition, :finish)
+      params.require(:instrument).permit(:title, :description, :model, :brand, :price, :condition, :finish, :image)
     end
 end
--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,2 +1,9 @@
 class Instrument < ApplicationRecord
+  belongs_to :user
+  mount_uploader :image, ImageUploader
+
+  BRAND = %w{ Fender Gibson Epiphone ESP Martin Dean Taylor Jackson PRS  Ibanez Charvel Washburn }
+  FINISH = %w{ Black White Navy Blue Red Clear Satin Yellow Seafoam }
+  CONDITION = %w{ New Excellent Mint Used Fair Poor }
+
 end
